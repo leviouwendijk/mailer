4bcb86e092088553972d22c1ff2b39467e43bbc9
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-05-14 21:01:30 +0200

    update in plate dependency requiring higher macos version for compat

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "mailer",
+    platforms: [
+        .macOS(.v13)
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/leviouwendijk/plate.git", branch: "master"),
